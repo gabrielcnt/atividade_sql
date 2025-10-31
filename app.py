54b34d909fc4230b185fa002d11573931a684ffb
Gabriel Vieira <gabrielcnt96@gmail.com>
2025-10-31 18:05:34 -0300

    feat: inseri 5 livros de exemplo na tabela

--- app.py
+++ app.py
@@ -13,3 +13,18 @@ cursor.execute('''
         genero TEXT,
         disponivel INTEGER NOT NULL DEFAULT 1 CHECK (disponivel IN (0, 1)))
 ''')
+
+# inserir 5 livros
+cursor.executemany('''
+    INSERT OR IGNORE INTO livros (titulo, autor, ano, genero, disponivel)
+        VALUES (?, ?, ?, ?, ?)
+''',[
+    ('1984', 'George Orwell', 1949, 'Distopia / Ficção política', 1), 
+    ('Dom Casmurro', 'Machado de Assis', 1899, 'Romance psicológico / Romance brasileiro', 0),
+    ('O Senhor dos Anéis: A Sociedade do Anel', 'J. R. R. Tolkien', 1954, 'Fantasia / Aventura', 1), 
+    ('Orgulho e Preconceito', 'Jane Austen', 1813, 'Romance / Ficção clássica', 0), 
+    ('A Revolução dos Bichos', 'George Orwell', 1945, 'Sátira política / Fábula', 1)
+    ])
+
+banco.commit()
+banco.close()
